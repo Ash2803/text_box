daabb7e0e062d877b3f9890eaf7cdf7fd5008cb1
Ashot Abazyan <ashot.93@mail.ru>
2023-03-16 21:56:05 +0400

    Updated test

--- tests/text_box_test.py
+++ tests/text_box_test.py
@@ -1,15 +1,15 @@
 from pages.text_box_page import TextBoxPage
 
 
-class TestElements:
-    class TestBoxPage:
+class TestBoxPage:
 
-        def test_text_box(self, driver):
-            test_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
-            test_box_page.open_page()
-            full_name, email, current_address, perm_address = test_box_page.fill_fields()
-            output_full_name, output_email, output_current_address, output_perm_address = test_box_page.check_output()
-            assert full_name == output_full_name
-            assert email == output_email
-            assert current_address == output_current_address
-            assert perm_address == output_perm_address
+    def test_text_box(self, driver):
+        test_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
+        test_box_page.open_page()
+        full_name, email, current_address, perm_address = test_box_page.fill_fields()
+        output_full_name, output_email, output_current_address, output_perm_address = test_box_page.check_output()
+        assert full_name == output_full_name
+        assert email == output_email
+        assert current_address == output_current_address
+        assert perm_address == output_perm_address
+            
